163b55bd54ff0bf796e6f823cb6a0067c9ea440f
Gustavo Matos <37024361+guMatos@users.noreply.github.com>
2019-12-11 23:45:16 -0300

    Use body-parser in create person endpoint

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 const person = require('../models/person')
 
 const router = express.Router()
+router.use(bodyParser.urlencoded({ extended: false }))
+router.use(bodyParser.json())
 
 router.post('/create', (req, res) => {
 	var result = new person({
@@ -11,8 +14,8 @@ router.post('/create', (req, res) => {
 	})
 
 	result.save((err) => {
-		if (err) res.send('Could not save object to DB')
-		else { res.send('Object created succesfully') }
+		if (err) res.status(500).send('Could not save object to DB')
+		else res.send('Object created succesfully')
 	})
 })
 
