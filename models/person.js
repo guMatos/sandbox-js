c49a9178001166d42c553c6ac1da235e8ae12652
Gustavo Matos <37024361+guMatos@users.noreply.github.com>
2019-12-11 23:44:14 -0300

    Adds unique property to cpf field of person

--- models/person.js
+++ models/person.js
@@ -1,10 +1,9 @@
 import mongoose from 'mongoose'
-//const schema = mongoose.Schema()
 
 var personSchema = new mongoose.Schema({
 	name: { type: String, required: true, max: 100 },
 	age: { type: Number, required: true, max: 1000 },
-	cpf: { type: String, required: true, max: 11 }
+	cpf: { type: String, required: true, max: 11, unique: true, dropDups: true }
 })
 
 module.exports = mongoose.model('person', personSchema)
