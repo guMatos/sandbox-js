a79e96ca070b566ec55c5986a34cb0885b5d9458
Gustavo Matos <37024361+guMatos@users.noreply.github.com>
2019-12-14 19:12:38 -0300

    Adds search endpoint

--- routes/get.js
+++ routes/get.js
@@ -1,9 +1,19 @@
 import express from 'express'
+const person = require('../models/person')
 
 const router = express.Router()
 
 router.get('/get', (req, res) => {
 	res.send('api get')
+})
+
+router.get('/search/:cpf', (req, res) => {
+	var cpf = req.params.cpf
+
+	person.findOne({"cpf": cpf}).then(e => {
+		if (!e) res.status(404).send('Person with cpf ' + cpf + ' not found')
+		else res.send(e)
+	})
 })
 
 module.exports = router
