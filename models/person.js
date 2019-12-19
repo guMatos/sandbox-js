0b19abba2fafe58ed5c3623acb2fffaa44368e2b
Gustavo Matos <37024361+guMatos@users.noreply.github.com>
2019-12-19 00:08:50 -0300

    Updates length validation of string properties

--- models/person.js
+++ models/person.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose'
 
 var personSchema = new mongoose.Schema({
-	name: { type: String, required: true, max: 100 },
+	name: { type: String, required: true, maxlength: 100, minlength: 1 },
 	age: { type: Number, required: true, max: 1000 },
-	cpf: { type: String, required: true, max: 11, unique: true, dropDups: true }
+	cpf: { type: String, required: true, maxlength: 11, minlength: 11, unique: true, dropDups: true }
 })
 
 module.exports = mongoose.model('person', personSchema)
