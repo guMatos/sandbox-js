1f42fec579b06d98e6c9a80c47291f04eb290af3
Gustavo Matos <37024361+guMatos@users.noreply.github.com>
2019-12-20 19:18:27 -0300

    Rename Person schema and changes order of validations

--- routes/put.js
+++ routes/put.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-const person = require('../models/person')
+const Person = require('../models/person')
 
 const router = express.Router()
 router.use(bodyParser.urlencoded({ extended: false }))
@@ -14,9 +14,9 @@ router.put('/update', (req, res) => {
 		age: req.body.person.age
 	}
 
-	person.findOneAndUpdate(filter, update, { runValidators: true }, (err, doc) => {
-		if (!doc) return res.status(404).send('Person with cpf "' + filter.cpf + '" not found')
+	Person.findOneAndUpdate(filter, update, { runValidators: true }, (err, doc) => {
 		if (err) return res.status(500).send({error: err})
+		if (!doc) return res.status(404).send('Person with cpf "' + filter.cpf + '" not found')
 		return res.send('Record successfully updated')
 	})
 })
